bc2de18954d735cb44fec5dd4dec64cb95be185b
James Swineson <github@public.swineson.me>
2020-05-13 00:41:26 +0800

    use docker inside pipeline

---
+++ README.md
@@ -0,0 +1,6 @@
+# vpp-buildbot
+
+Experimental CI build for [FD.io VPP](https://fd.io/) on Debian 10.
+
+[![Build Status](https://dev.azure.com/nekomimiswitch/General/_apis/build/status/vpp-buildbot?branchName=master)](https://dev.azure.com/nekomimiswitch/General/_build/latest?definitionId=87&branchName=master)
+
--- build.sh
+++ build.sh
@@ -1,28 +1,10 @@
 #!/bin/bash
-# set -Eeuo pipefail
+set -Eeuo pipefail
 set -x
 
-MAKE_ARGS="UNATTENDED=y V=${MAKE_VERBOSE} PLATFORM=${MAKE_PLATFORM} TAG=${MAKE_TAG}"
-declare -a VPPSB_PLUGINS=(
-	"RXTXRPT"
-)
-
-apt-get update -y
-apt-get install -y git build-essential
-
-git clone https://github.com/FDio/vpp.git
-git clone https://gerrit.fd.io/r/vppsb
-
-cd vpp
-git checkout "${VPP_BRANCH}"
-make "${MAKE_ARGS}" install-dep
-make "${MAKE_ARGS}" install-ext-deps
-make "${MAKE_ARGS}" build-release
-make "${MAKE_ARGS}" pkg-deb
-make "${MAKE_ARGS}" vom-pkg-deb
-
-for PLUGIN in "${VPPSB_PLUGINS[@]}"; do
-    ln -sf "../vppsb/${PLUGIN}"
-    ln -sf "../../../${PLUGIN}/turbotap.mk" "build-data/packages/"
-    make "${MAKE_ARGS}" ${PLUGIN}-install
-done
+docker run \
+    -v "$(pwd)":/root \
+    --workdir "/root" \
+    --name vpp-builder \
+    --env-file <( env| cut -f1 -d= ) \
+    $(CONTAINER) bash /root/build_docker.sh
