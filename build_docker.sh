8c636f532be5198f91f3ad42ea879714578ae0d8
James Swineson <github@public.swineson.me>
2020-05-13 11:19:32 +0800

    update plugin build

--- build_docker.sh
+++ build_docker.sh
@@ -23,11 +23,6 @@ git clone "${GIT_VPPSB_URL}" "vppsb"
 
 cd vpp
 git checkout "${VPP_BRANCH}"
-make "${MAKE_ARGS}" install-dep
-make "${MAKE_ARGS}" install-ext-deps
-make "${MAKE_ARGS}" build-release
-make "${MAKE_ARGS}" pkg-deb
-make "${MAKE_ARGS}" vom-pkg-deb || true # known to fail
 
 for PLUGIN in "${VPPSB_PLUGINS[@]}"; do
     # link necessary files in
@@ -35,15 +30,24 @@ for PLUGIN in "${VPPSB_PLUGINS[@]}"; do
     pushd build-data/packages/
     ln -sf "../../../${PLUGIN}/${PLUGIN}.mk"
     popd
+done
 
+make "${MAKE_ARGS}" install-dep
+make "${MAKE_ARGS}" install-ext-deps
+make "${MAKE_ARGS}" build-release
+
+for PLUGIN in "${VPPSB_PLUGINS[@]}"; do
     # build the plugin (let them fail)
-    pushd build-data/
+    pushd build-root/
     make "${MAKE_ARGS}" ${PLUGIN}-install || echo "WARNING: ${PLUGIN} build failed!"
     popd
 done
 
+make "${MAKE_ARGS}" pkg-deb
+make "${MAKE_ARGS}" vom-pkg-deb || true # known to fail
+
 # archive artifacts
 cd build-data/
-for DIRECTORY in build-*/ install-*; do
+for DIRECTORY in build-vpp-native install-vpp-native; do
     tar -xzf "${DIRECTORY}".tar.gz "${DIRECTORY}"
 done
